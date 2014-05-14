6ee99d03a30a3f2a5818d7fcad369645cc281562
Ercan Başa <ercan.basa@markafoni.com>
2014-05-14 11:05:28 +0300

    send mail shortcut is done

--- blog/utils.py
+++ blog/utils.py
@@ -2,7 +2,11 @@
 
 import requests
 import json
+
 from django.conf import settings
+from django.core.mail import send_mail
+from django.template.loader import render_to_string
+
 
 def get_count(api, url):
     """
@@ -15,3 +19,11 @@ def get_count(api, url):
     if social['key'] in json.loads(data):
         return json.loads(data)[social['key']]
     return 0
+
+
+def send_mail_with_template(subject=None, mail_to=None, template=None, context={}):
+    if not isinstance(mail_to, list):
+        mail_to = [mail_to]
+
+    body = render_to_string(template, context)
+    send_mail(subject, body, settings.EMAIL_HOST_USER, mail_to)
