4b9913c9a1382a3e6295b5c3e2550cd2005aac7b
Ercan Başa <ercan.basa@markafoni.com>
2014-05-23 13:58:34 +0300

    post admin fixed

--- blog/admin.py
+++ blog/admin.py
@@ -3,18 +3,17 @@ from django.contrib import admin
 from blog.models import Tag, Category, Post, Contact, Profile
 from blog.utils import send_mail_with_template
 
+
 class PostAdmin(admin.ModelAdmin):
     def save_model(self, request, obj, form, change):
         if change and request.user != obj.user and obj.is_active:
             send_mail_with_template(u'Yazın onaylandı!', obj.user.email,
                                     'mails/post-accepted.html')
-
-
-
+        super(PostAdmin, self).save_model(request, obj, form, change)
 
 
 admin.site.register(Tag)
 admin.site.register(Category)
-admin.site.register(Post)
+admin.site.register(Post, PostAdmin)
 admin.site.register(Contact)
 admin.site.register(Profile)
